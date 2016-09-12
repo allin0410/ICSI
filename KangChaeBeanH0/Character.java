a0c2fdf3dba958bb30c4dcad155decf7a4485009
allin0410 <chaebeankang@dyn-169-226-217-104.wireless.albany.edu>
2016-09-12 15:26:55 -0400

    Establishes properties for Character class

---
+++ KangChaeBeanH0/Character.java
@@ -0,0 +1,9 @@
+public class Character {
+  // Properties:
+  private int[] stats; // HP, strength, mana
+  // current location of Character, x, y coordinate
+  private int[] location;
+  // location where Character stars: x,y coordinate
+  private String[] skills;
+  private int[] spawnLocation;//x,y coordinate
+}
